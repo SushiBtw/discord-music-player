02c1d9052a054fda2a64324b9d74a4c442373fa8
SushiBtw <kalinowskiadam2003@gmail.com>
2020-11-25 20:08:38 +0100

    Workflows/Tests, none special

--- test/test.js
+++ test/test.js
@@ -1,8 +1,6 @@
 let TestStrings = [
     'Never Gonna Give You Up',
-    'This is the Life'
 ]
-
 
 const Discord = require("discord.js");
 const client = new Discord.Client();
@@ -14,10 +12,11 @@ const ytsr = require('../src/node-ytsr-wip/main');
 
 for (var String in TestStrings) {
 
-    console.log(String);
+    console.log(`Running Song: ${TestStrings[String]}`);
 
     Util.getFirstSearch(TestStrings[String], ytsr).then(async result => {
-        console.log(result.title);
+        console.log(`Found Song: ${result.title}`);
+        process.exit(0);
     });
 
 }
