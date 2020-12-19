a812c51eb22be1298c5df5f79aeafe17db8bc939
SushiBtw <kalinowskiadam2003@gmail.com>
2020-12-19 22:43:23 +0100

    Add minimum versioning (NodeJS)

--- src/Player.js
+++ src/Player.js
@@ -3,6 +3,9 @@ const mergeOptions = require('merge-options');
 const ytsr = require('./node-ytsr-wip/main');
 const { VoiceChannel, version } = require("discord.js");
 if (version.split('.')[0] !== '12') throw new Error("Only the master branch of discord.js library is supported for now. Install it using 'npm install discordjs/discord.js'.");
+
+if (process.version.split('.')[0].substring(1) < 14) throw new Error("Discord.js requires NodeJS version >= 14.0.0, for Music Handlers now. Please update your Node at https://nodejs.org/en/.");
+
 const Queue = require('./Queue');
 const Util = require('./Util');
 const Song = require('./Song');
@@ -70,8 +73,6 @@ class Player {
                 // If the channel is not empty
                 if (queue.connection.channel.members.size > 1) return;
                 // Start timeout
-
-                console.log(this.options.timeout);
 
                 setTimeout(() => {
                     // If the channel is not empty
