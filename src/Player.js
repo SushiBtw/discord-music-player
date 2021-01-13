135a2f03288c0835d9913e4f49422eaca3667f02
SpeckyYT <Specky.fans@gmail.com>
2021-01-13 21:07:29 +0100

    Update Player.js

--- src/Player.js
+++ src/Player.js
@@ -103,7 +103,7 @@ class Player {
      */
     async play(voiceChannel, songName, options = {}, requestedBy) {
         this.queues = this.queues.filter((g) => g.guildID !== voiceChannel.id);
-        if (voiceChannel?.type !== 'voice' ?? true) return new MusicPlayerError('VoiceChannelTypeInvalid', 'song');
+        if (voiceChannel.type ? voiceChannel.type !== 'voice' : true) return new MusicPlayerError('VoiceChannelTypeInvalid', 'song');
         if (typeof songName !== 'string' || songName.length == 0) return new MusicPlayerError('SongTypeInvalid', 'song');
         if (typeof options !== 'object') return new MusicPlayerError('OptionsTypeInvalid', 'song');
         try {
@@ -141,14 +141,14 @@ class Player {
      */
     async playlist(guildID, playlistLink, voiceChannel, maxSongs, requestedBy) {
         let queue = this.queues.find((g) => g.guildID === guildID);
-        if (!queue) if (voiceChannel?.type !== 'voice' ?? true) return new MusicPlayerError('VoiceChannelTypeInvalid', 'song', 'playlist');
+        if (!queue) if (voiceChannel.type ? voiceChannel.type !== 'voice' : true) return new MusicPlayerError('VoiceChannelTypeInvalid', 'song', 'playlist');
         if (typeof playlistLink !== 'string' || playlistLink.length == 0) return new MusicPlayerError('PlaylistTypeInvalid', 'song', 'playlist');
         if (typeof maxSongs !== 'number') return new MusicPlayerError('MaxSongsTypeInvalid', 'song', 'playlist');
 
         try {
             // Searches the playlist
             let playlist = await Util.getVideoFromPlaylist(playlistLink, ytsr, maxSongs);
-            let connection = queue?.connection ?? null;
+            let connection = queue.connection;
             let isFirstPlay = queue ? true : false;
             let playlistSongs = [];
 
